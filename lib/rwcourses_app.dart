df08b2539b1fab1186ff070936c284c2224129ac
BakytDzhumabaev <djumabaevb@gmail.com>
2021-12-30 14:08:40 +0600

    navigation to filter page

--- lib/rwcourses_app.dart
+++ lib/rwcourses_app.dart
@@ -1,6 +1,9 @@
+
+   
 import 'package:flutter/material.dart';
 import 'strings.dart';
 import 'ui/courses/courses_page.dart';
+import 'ui/filter/filter_page.dart';
 
 class RWCoursesApp extends StatelessWidget {
   const RWCoursesApp({Key? key}) : super(key: key);
@@ -10,6 +13,14 @@ class RWCoursesApp extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(Strings.appTitle),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.filter_list),
+            onPressed: () => Navigator.of(context).push<MaterialPageRoute>(
+              MaterialPageRoute(builder: (_) => const FilterPage()),
+            ),
+          )
+        ],
       ),
       body: const CoursesPage(),
     );

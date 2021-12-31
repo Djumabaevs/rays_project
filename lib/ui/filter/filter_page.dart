b9411d311cee239e67c973b4f40039127b7bf7e4
BakytDzhumabaev <djumabaevb@gmail.com>
2021-12-31 14:59:31 +0600

    filter page

--- lib/ui/filter/filter_page.dart
+++ lib/ui/filter/filter_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:rays_project/constants.dart';
+import 'package:rays_project/ui/filter/filter_widget.dart';
 import '../../strings.dart';
 
 class FilterPage extends StatefulWidget {
@@ -9,13 +11,45 @@ class FilterPage extends StatefulWidget {
 }
 
 class _FilterPageState extends State<FilterPage> {
+  int _filterValue = Constants.allFilter;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(Strings.filter),
       ),
-      body: const Text('Filter'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          FilterWidget(
+              value: Constants.iosFilter,
+              groupValue: _filterValue,
+              onChanged: _handleRadioValueChange,
+              text: Strings.ios),
+          FilterWidget(
+              value: Constants.androidFilter,
+              groupValue: _filterValue,
+              onChanged: _handleRadioValueChange,
+              text: Strings.android),
+          FilterWidget(
+              value: Constants.sssFilter,
+              groupValue: _filterValue,
+              onChanged: _handleRadioValueChange,
+              text: Strings.sss),
+          FilterWidget(
+              value: Constants.allFilter,
+              groupValue: _filterValue,
+              onChanged: _handleRadioValueChange,
+              text: Strings.all),
+        ],
+      ),
     );
+  }
+
+  void _handleRadioValueChange(int? value) {
+    setState(() {
+      _filterValue = value ?? 0;
+    });
   }
 }
--- lib/ui/filter/filter_widget.dart
+++ lib/ui/filter/filter_widget.dart
@@ -6,17 +6,24 @@ class FilterWidget extends StatelessWidget {
   final ValueChanged<int?> onChanged;
   final String text;
 
-  const FilterWidget({ 
-    Key? key,
-    required this.value,
-    required this.groupValue,
-    required this.onChanged,
-    required this.text }) : super(key: key);
+  const FilterWidget(
+      {Key? key,
+      required this.value,
+      required this.groupValue,
+      required this.onChanged,
+      required this.text})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Row(
+      children: [
+        Radio(value: value, groupValue: groupValue, onChanged: onChanged),
+        Text(
+          text,
+          style: const TextStyle(fontSize: 16.0),
+        ),
+      ],
     );
   }
 }
